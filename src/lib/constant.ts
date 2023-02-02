821908130c892c1a9f429e4bc6c23f8940b8f371
dennis ko <dennis0324@naver.com>
2023-02-03 01:00:24 +0900

    add: tableType for database table

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -29,3 +29,23 @@ export const availableType = [
   "transclusion_reference",
   "collection_view"
 ]
+
+export const tableType = [
+  "date", //text
+  "status", //text
+  "select", //text
+  "number", //text
+  "phone_number", //text
+  "multi_select", //text
+  "email", // text,
+  "file", // ??
+  "person", // ??
+  "created_time", //need process
+  "checkbox", //need process && backend edited
+  "created_by", //need process
+  "last_edited_by", //need process
+  "formula", //need process
+  "rollup", //need process
+  "url", // edited required
+  "last_edited_time", //need process
+]
