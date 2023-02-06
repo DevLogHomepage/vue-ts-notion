639c2498415753662635ac5267263d033ebc448d
dennis ko <dennis0324@naver.com>
2023-02-06 23:34:25 +0900

    remove: getText function

--- src/blocks/helpers/database-table-cell.vue
+++ src/blocks/helpers/database-table-cell.vue
@@ -1,40 +1,31 @@
 <script setup lang="ts">
-import { computed, defineComponent, PropType } from 'vue'
-import type { DecorationType, BlockValue, TableType, ColumnSchemaType, tableValueProperties, TableBlockProperties } from '@/lib/types';
+import {  defineComponent, PropType } from 'vue'
+import type {  ColumnSchemaType, tableValueProperties } from '@/lib/types';
 import { defineNotionProps,useNotionBlock } from '@/lib/blockable';
-import { useMath } from '@/lib/math'
-import NotionTextRenderer from '@/blocks/helpers/text-renderer.vue'
-import { useDatabase } from '@/lib/database';
+import { defineDatabaseProps, useDatabase } from '@/lib/database';
 import CheckBoxIcon from './check-box-icon.vue';
 // import { formula } from '@/lib/math';
 
 const props = defineProps({
-    collectionData:Object as PropType<ColumnSchemaType>,
     data:Object as PropType<tableValueProperties>,
-    setRef:Function, 
+    schemaData:Object as PropType<ColumnSchemaType>,
+    ...defineDatabaseProps,
     ...defineNotionProps 
 })
 
 //@ts-ignore
 const { blockMap,pass } = useNotionBlock(props)
 //@ts-ignore
-const { schema } = useDatabase(props)
-//@ts-ignore
-const { formula } = useMath(props)
+const { getDBTable } = useDatabase(props)
+
 
 
 const type = (t:string | string[]) => {
-    console.log(props.collectionData?.type)
     if(Array.isArray(t)) return t.includes(props.collectionData!.type)
     return props.collectionData!.type === t
 }
 
-const getText = computed(() => {
-    if(!props.data) return [[props.collectionData?.name]] as DecorationType[]
-    console.log((props.data as tableValueProperties)[props.collectionData!.name][0][0])
-    return ((props.data as tableValueProperties)[props.collectionData!.name] ?? [[' ']]  )as DecorationType[]
-})
-const isTrue = computed(() => {return getText.value[0][0] === 'Yes'})
+// const isTrue = computed(() => {return getText.value[0][0] === 'Yes'})
 </script>
 
 
@@ -49,15 +40,13 @@ export default defineComponent({
 <template>
     <div class="notion-database-table-cell">
         <div class="notion-database-table-text">
-            <NotionTextRenderer v-if="!data" v-bind="pass" :text="getText"/>
-            <!-- text cell -->
+
+            {{ getDBTable(props.schemaData as ColumnSchemaType,props.data as tableValueProperties) }}
+            <!-- <NotionTextRenderer v-if="!data" v-bind="pass" :text="getText"/>
             <NotionTextRenderer v-else-if="type(['date','status','select','number','phone_number','multi_select','email'])" v-bind="pass" :text="getText"/>
-            <!-- checkbox -->
             <CheckBoxIcon v-else-if="type('checkbox')" :class="{'checkbox-true':isTrue}"/>
-            <!-- formula cell -->
             <div></div>
-            <!-- rollup -->
-            <div></div>
+            <div></div> -->
         </div>
     </div>
 </template>
